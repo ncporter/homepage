56935d2fac10490c22d86c009ae1bffe6216e93e
ncporter <74282675+ncporter@users.noreply.github.com>
2023-07-22 11:15:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "merry christmas!"
+    return "this is a website"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
